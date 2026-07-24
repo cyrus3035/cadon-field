5d83958a4aca3104876349bb749a35a2d0bca1b0
cyrus3035 <cyrus3035@github.com>
2026-07-24 16:39:57 +0800

    fix: convert index.js back to CJS syntax for react-native bundle compatibility

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { registerRootComponent } from 'expo';
-import App from './src/App';
+const { registerRootComponent } = require('expo');
+const App = require('./src/App').default;
 
 registerRootComponent(App);
